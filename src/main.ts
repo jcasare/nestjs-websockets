4af40568a1fa0b84880ac66e94ec88470737adfc
jcasare <asarejerry16@gmail.com>
2024-06-19 16:00:43 +0000

    scripts modify

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: '*', methods: '*' });
+  await app.listen(PORT);
 }
 bootstrap();
